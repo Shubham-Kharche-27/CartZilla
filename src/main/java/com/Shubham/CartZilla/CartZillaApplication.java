a05dacf41c624fb080ea7e4a606e46d97fd551ac
Shubham <shubhamkharche2003@gmail.com>
2025-08-08 15:10:20 +0530

    Ecommerce basic project setup completed

---
+++ src/main/java/com/Shubham/CartZilla/CartZillaApplication.java
@@ -0,0 +1,13 @@
+package com.Shubham.CartZilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartZillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartZillaApplication.class, args);
+	}
+
+}
